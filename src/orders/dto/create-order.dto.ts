3896406d5e0d2bf6b37406ecbba5845f16000441
Alex <b514t014@gmail.com>
2024-03-11 23:38:38 +0200

    update orders schemas and dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,35 +1,17 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 
-export class GeneralDetails {
-  readonly date: string;
-  readonly timeFrom: string;
-  readonly timeTo: string;
-  readonly address: string;
-  readonly postCode: string;
-  readonly country: string;
-  readonly distance: number;
-  readonly coordinates: string;
-  readonly cargoDetails: string;
-  readonly comments: string;
-}
-
 export class LoadingDetails {
-  readonly date: string;
-  readonly timeFrom: string;
-  readonly timeTo: string;
+  readonly index: string;
+  readonly dateTime: string;
   readonly address: string;
-  readonly postCode: string;
-  readonly country: string;
-  readonly distance: number;
   readonly coordinates: string;
-  readonly cargoDetails: string;
   readonly comments: string;
-  readonly unloadingPlace: GeneralDetails;
+  readonly distance: number;
 }
 
 export class CreateOrderDto {
   @IsNotEmpty()
-  readonly truckNumber: string; 
+  readonly truckNumber: string;
   @IsNotEmpty()
   readonly order: LoadingDetails;
 }
--- src/orders/schemas/loadingDetails.schemas.ts
+++ src/orders/schemas/loadingDetails.schemas.ts
@@ -1,34 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { GeneralDetails, GeneralDetailsSchema } from './generalDetails.schemas';
 
 @Schema({ _id: false })
 export class LoadingDetails {
   @Prop()
-  index: number
+  index: number;
+  @Prop()
+  type: string;
   @Prop()
   dateTime: string;
   @Prop()
-  timeFrom: string;
-  @Prop()
-  timeTo: string;
-  @Prop()
   address: string;
-  @Prop()
-  postCode: string;
-  @Prop()
-  country: string;
-  @Prop()
-  distance: number;
-  @Prop()
-  coordinates: string;
-  @Prop()
-  cargoDetails: string;
   @Prop()
   comments: string;
   @Prop()
-  type: string;
-  // @Prop({ type: [GeneralDetailsSchema], default: [] })
-  // unloadingPlace: [GeneralDetails];
+  coordinates: string;
+  @Prop()
+  distance: number;
 }
 
 export const LoadingDetailsSchema =
